716485f4f535fafd8c0c045ef0233aa106137fa3
microcai <microcaicai@gmail.com>
2018-04-09 17:54:13 +0800

    work arround windows sdk bug

--- src/qt/winshutdownmonitor.cpp
+++ src/qt/winshutdownmonitor.cpp
@@ -2,13 +2,21 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#ifdef _WIN32
+
+#include <windows.h>
+
+#endif
+
+//#define _AMD64_
 #include <qt/winshutdownmonitor.h>
 
 #if defined(Q_OS_WIN) && QT_VERSION >= 0x050000
-#include <init.h>
-#include <util.h>
 
 #include <windows.h>
+
+#include <init.h>
+#include <util.h>
 
 #include <QDebug>
 
