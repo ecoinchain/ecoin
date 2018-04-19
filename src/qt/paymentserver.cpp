cca2cf48b16854e7b9097a6f93404dcefaf5f362
microcai <microcaicai@gmail.com>
2018-04-19 21:30:09 +0800

    fix open uri

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -49,7 +49,7 @@
 #endif
 
 const int BITCOIN_IPC_CONNECT_TIMEOUT = 1000; // milliseconds
-const QString BITCOIN_IPC_PREFIX("bitcoin:");
+const QString BITCOIN_IPC_PREFIX(QAPP_COIN_NAME_URI_SCHEME);
 // BIP70 payment protocol messages
 const char* BIP70_MESSAGE_PAYMENTACK = "PaymentACK";
 const char* BIP70_MESSAGE_PAYMENTREQUEST = "PaymentRequest";
