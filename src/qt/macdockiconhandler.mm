183c4a2943569b86cd54494ebcada6ebe9ed8132
microcai <microcaicai@gmail.com>
2018-04-23 16:06:36 +0800

    ensure  q_os_mac is defined

--- src/qt/macdockiconhandler.mm
+++ src/qt/macdockiconhandler.mm
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#define Q_OS_MAC
 #include "macdockiconhandler.h"
 
 #include <QImageWriter>
