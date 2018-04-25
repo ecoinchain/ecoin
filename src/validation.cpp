754b95be1bda6d35131c2dab0db815122b8ef9d4
microcai <microcaicai@gmail.com>
2018-04-25 16:46:29 +0800

    fix block.nBits compare

--- src/validation.cpp
+++ src/validation.cpp
@@ -3120,7 +3120,7 @@ static bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationSta
 
     // Check proof of work
     const Consensus::Params& consensusParams = params.GetConsensus();
-    if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
+    if (block.nBits > GetNextWorkRequired(pindexPrev, &block, consensusParams))
         return state.DoS(100, false, REJECT_INVALID, "bad-diffbits", false, "incorrect proof of work");
 
     // Check against checkpoints
