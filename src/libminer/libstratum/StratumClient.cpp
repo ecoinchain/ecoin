7737c257dd320f19815f207c91193b1d476306c5
microcai <microcaicai@gmail.com>
2018-06-21 17:29:07 +0800

    remove GPU solver.

--- src/libminer/libstratum/StratumClient.cpp
+++ src/libminer/libstratum/StratumClient.cpp
@@ -317,8 +317,8 @@ void StratumClient<Miner, Job, Solution>::workLoop(boost::system::error_code ec,
 						const Object& responseObject = valResponse.get_obj();
 						if (!responseObject.empty())
 						{
-							processReponse(responseObject);
 							m_response = response;
+							processReponse(responseObject);
 						}
 						else
 						{
